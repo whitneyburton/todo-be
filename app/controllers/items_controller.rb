a9f0e1cd952bdd821619655f13263489de9d0325
Whitney Burton <whitneyburton9@gmail.com>
2019-05-11 11:03:15 -0600

    Add GET /todos/:todo_id/items to ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :set_todo
+  before_action :set_todo_item, only: [:show, :update, :destroy]
+
+  # GET /todos/:todo_id/items
+  def index 
+    json_response(@todo_items)
+  end
 end

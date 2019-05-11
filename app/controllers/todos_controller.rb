65d3159cc79bfff38784cd972fc99900b78676d3
Whitney Burton <whitneyburton9@gmail.com>
2019-05-11 11:03:29 -0600

    Add GET /todos to TodosController

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,9 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  # GET /todos
+  def index
+    @todos = Todo.all
+    json_response(@todos)
+  end
 end

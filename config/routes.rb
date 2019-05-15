b547cebb1d3af9e4d5c3c799fe898c74025bc73b
Whitney Burton <whitneyburton9@gmail.com>
2019-05-15 15:24:34 -0600

    Add Sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # devise_for :users
   namespace :api do
     namespace :v1 do
       resources :todos do
         resources :items
       end 
+      resources :sessions, only: [:create, :destroy]
     end
   end 
 end

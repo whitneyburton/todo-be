eec0592d9ea228983d5c8976650b45fc2d6f9490
Whitney Burton <whitneyburton9@gmail.com>
2019-05-15 11:07:29 -0600

    Add delete method to CORS exceptions

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module TodoBe
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.

9ac632fba108b144c47ec7e7080087d66df888da
Whitney Burton <whitneyburton9@gmail.com>
2019-05-16 12:02:59 -0600

    Add sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,25 @@
 class Api::V1::SessionsController < ApplicationController
+  def index
+    @session = Session.all
+    json_response(@session)
+  end
+
   def create 
+    user = User.where(email: params[:email]).first
+
+    if user&.valid_password?(params[:password])
+      render json: user.as_json(only: [:email, :authentication_token, :id]), status: :created
+    else 
+      head[:unauthorized]
+    end
   end
   
   def destroy
+    current_user&.authentication_token = nil
+    if current_user.save
+      head(:ok)
+    else  
+      head(:unauthorized)
+    end
   end
 end

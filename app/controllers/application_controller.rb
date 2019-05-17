5fd1012ba2c50c4a5dc12b35de54b798f53659e1
Whitney Burton <whitneyburton9@gmail.com>
2019-05-16 23:05:15 -0600

    Add helper method functionality to ApplicationController and current_user variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
+  helper_method :current_user 
   acts_as_token_authentication_handler_for User, fallback: :none
   include Response
   include ExceptionHandler
+
+  private
+ 
+  def current_user
+   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end

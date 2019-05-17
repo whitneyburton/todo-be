349bed8c256041e1efb558ff8d0912b66f8d89e5
Whitney Burton <whitneyburton9@gmail.com>
2019-05-17 03:24:42 -0600

    Adjust sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::SessionsController < ApplicationController
 
   def create 
     user = User.where(email: params[:email]).first
-
+    
     if user&.valid_password?(params[:password])
       render json: user.as_json(only: [:email, :authentication_token, :id]), status: :created
     else 

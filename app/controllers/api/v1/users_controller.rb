c8bc1bc279181b8414718ff2a6fe0080bb568e5d
Whitney Burton <whitneyburton9@gmail.com>
2019-05-16 23:04:42 -0600

    WIP: Add create method for users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,10 +10,20 @@ class Api::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     json_response(user)
   end
+
+  def create 
+    user = User.new(user_params)
+
+    if user.save
+      render :create
+    else  
+      head(:unprocessable_entity)
+    end
+  end
   
   private 
 
   def user_params
-    params.permit(:title, :created_by)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
